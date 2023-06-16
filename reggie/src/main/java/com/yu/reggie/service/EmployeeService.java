5e014a130dfd4f7204c5e3218b826c63a0f1a1ee
Yangzeng <2045721638@qq.com>
2023-06-16 17:52:55 +0800

    yangz

---
+++ reggie/src/main/java/com/yu/reggie/service/EmployeeService.java
@@ -0,0 +1,7 @@
+package com.yu.reggie.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.yu.reggie.entity.Employee;
+
+public interface EmployeeService extends IService<Employee> {
+}
