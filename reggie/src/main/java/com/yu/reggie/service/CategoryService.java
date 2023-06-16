1b46eba487199329111cfb73955b4090d0fb1bf2
lixuan <451002465@qq.com>
2023-06-16 18:26:21 +0800

    提交

---
+++ reggie/src/main/java/com/yu/reggie/service/CategoryService.java
@@ -0,0 +1,13 @@
+package com.yu.reggie.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.yu.reggie.entity.Category;
+
+public interface CategoryService extends IService<Category> {
+
+    /**
+     * 根据id删除菜品种类
+     * @param id
+     */
+    public void remove(Long id);
+}
